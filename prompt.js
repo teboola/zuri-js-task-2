ead34fbf708bd19059fa51618f2f2f426c9a18c4
Oredein Oluwatoyin <104014691+teboola@users.noreply.github.com>
2022-06-28 13:18:17 +0100

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -1,29 +1,23 @@
 function myFunction() {
   var x,y,z,oper;
-  x=prompt("Type first number");
-  y=prompt("Type second number");
-  oper=prompt("Type an operator");
+  x=prompt("Enter First Number");
+  oper=prompt("Enter an Operator");
+  y=prompt("Enter Second Number");
   
   if(oper=="+"){
     z=Number(x)+Number(y);
   }
-  else if(oper=!null){
-    z="Error"
+  if(oper=="-"){
+    z=Number(x)-Number(y);
   }
-  else if(x=!null){
-    z="Error"
-  }
-  else if(y=!null){
-    z="Error"
-  }
-  else if(oper=="/"){
-    z=Number(x)/Number(y);
-  }
-  else if(oper=="*"){
+  if(oper=="*"){
     z=Number(x)*Number(y);
   }
-  else if(oper=="-"){
-    z=Number(x)-Number(y);
+  if(oper=="/"){
+    z=Number(x)/Number(y);
+  }
+  if(oper=="%"){
+    z=Number(x)%Number(y);
   }
   
   return z;
